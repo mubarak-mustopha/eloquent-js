f7cf8adaf0a4b03a2791b0ff486c03d54e3312cd
mubaarock <mustophamubarack17@gmail.com>
2024-05-19 01:55:32 +0100

    Iteration over character code with for/of loop and over code units with traditional for loop

---
+++ higher-order-funcs/script/charCode.js
@@ -0,0 +1,10 @@
+let horseShoe = "🐴👟";
+let roseDragon = "🌹🐉";
+
+for (let char of roseDragon){
+    console.log(char);
+}
+
+for (let i=0; i < horseShoe.length; i++){
+    console.log(horseShoe[i]);
+}
