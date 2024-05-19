72bac4eb9e0e8ca2048ab9f4987ab56287434c34
mubaarock <mustophamubarack17@gmail.com>
2024-05-19 01:52:01 +0100

    .

--- higher-order-funcs/practice.js
+++ higher-order-funcs/practice.js
@@ -1,19 +1,15 @@
-function repeat(n, action){
-    for (let i = 0;i < n;i++){
-        action(i);
+function repeat(n, func){
+    for (let i = 0; i<n; i++){
+        func(i)
     }
 }
 
 function unless(test, then){
-    if (!test){
-        then();
-    }
+    if (!test) then();
 }
 
-repeat(4,console.log)
+repeat(4, console.log)
 
-unless( (82%2) == 1, () => console.log("It is even.") )
-
-repeat(4, (n) => {
-    unless( (n % 2) == 1, () => console.log(n))
+repeat(11, n => {
+    unless(n % 2 == 1, () => console.log(`${n} is even`) )
 })
