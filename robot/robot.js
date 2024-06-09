eaca2ac4161ce5c44ad13e13dfb219ac8611fd2b
mubaarock <mustophamubarack17@gmail.com>
2024-06-09 23:29:02 +0100

    Goal oriented robot

--- robot/robot.js
+++ robot/robot.js
@@ -78,6 +78,32 @@ VillageState.random = function(parcelCount = 5){
     }
  }
 
+// goal oriented robot
+function findRoute(graph, from, to){
+    let work = [{at: from, route: []}];
+    for (let i = 0; i < work.length ; i++){
+        let {at, route} = work[i];
+        for (let place of graph[at]){
+            if (place == to) return route.concat(place);
+            if (! work.some(w => w.at == place)){
+                work.push({at: place, route: route.concat(place)});
+            }
+        }
+    }
+}
+
+function goalOrientedRobot({place, parcels}, route){
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place){
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return {direction: route[0], memory: route.slice(1)};
+}
+
 // route robot
 const mailRoute = [
     "Alice's House", "Cabin", "Alice's House", "Bob's House",
@@ -102,7 +128,7 @@ function routeRobot(state, memory){
  }
 
 
-runRobot(VillageState.random(), routeRobot, []);
+runRobot(VillageState.random(parcelCount=25), goalOrientedRobot, []);
 
 // let first = new VillageState(
 //     "Post Office",
